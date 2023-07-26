0802362ad0baa5f2e482ff1308d0a007a10c5943
JeoungIN <shjin4667@gmail.com>
2023-07-26 09:50:23 +0900

    Create In_13.java

---
+++ chap05/In_13.java
@@ -0,0 +1,43 @@
+package chap05;
+
+interface Shape{
+	final double PI = 3.14;
+	void draw();
+	double getArea();
+	default public void redraw() {
+		System.out.println("---다시 그립니다---");
+		draw();
+	}
+}
+
+class Circle implements Shape{
+	private int radius;
+	
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("반지름이 " + radius + "인 원입니다.");
+	}
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return radius * radius * PI;
+	}
+	
+}
+public class In_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shape donut = new Circle(10);
+		donut.redraw();
+		System.out.println("면적은 " + donut.getArea());
+
+	}
+
+}
